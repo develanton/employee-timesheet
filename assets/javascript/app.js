536615c4f07936d8bc9d1c40e6ed139447863ef1
IsraelMuCa <isra.munoz@gmail.com>
2018-05-26 11:08:24 -0500

    added some JS to make the tabs work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+
+    //Catch containers
+    var addEmployeeContainer = $("#add-employee-container");
+    var seeEmployeeContainer = $("#see-employee-container");
+
+    //Hide containers
+    addEmployeeContainer.hide();
+    seeEmployeeContainer.show();
+
+    //Function to switch tabs
+    $('.tabs li').on('click', function() {
+        var tabClicked = $(this);
+        
+        if (tabClicked[0].id == "add-employee-list") {
+            tabClicked.addClass("is-active");
+            $("#see-employee-list").removeClass("is-active");
+            seeEmployeeContainer.hide();
+            addEmployeeContainer.show();
+
+        } else if (tabClicked[0].id == "see-employee-list") {
+            tabClicked.addClass("is-active");
+            $("#add-employee-list").removeClass("is-active");
+            addEmployeeContainer.hide();
+            seeEmployeeContainer.show();
+        }
+    });
+
+
     var config = {
         apiKey: "AIzaSyCWTt0IfvbOLlGSg7Kgj6IN4JfNA5Brje0",
         authDomain: "myproject-49aba.firebaseapp.com",
